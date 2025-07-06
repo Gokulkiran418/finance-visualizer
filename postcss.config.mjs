0c55ca23155d2ea04eba2c3a4b1aac099efc5b88
Gokulkiran418 <umgokulkiran@gmail.com>
2025-07-06 09:59:36 +0530

    set up tailwind postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
+// postcss.config.mjs
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    '@tailwindcss/postcss': {}, // Use the PostCSS plugin for Tailwind CSS v4
+    autoprefixer: {},
+  },
 };
 
 export default config;
